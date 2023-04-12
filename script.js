4ec195ea4a99013231d203820310d2311ab2ce4b
Thiago Rodrigues Sales <43177777+thiagornep@users.noreply.github.com>
2023-04-11 22:16:23 -0400

    comentários

--- script.js
+++ script.js
@@ -1,28 +1,27 @@
 function verificar() {
   window.alert("Testando a conexão....")
-
+  //recebendo as informações do input
   let dinheiro = document.getElementById("dinheiroemmaos")
   dinheiro = Number(dinheiro.value)
-
+  //recebendo as informações do input
   let ifood = document.getElementById("ifoodemmaos")
   ifood = Number(ifood.value)
-
+  //recebendo as informações do input
   let pix = document.getElementById("pixemmaos")
   pix = Number(pix.value)
-
+  //recebendo as informações do input
   let sumup = document.getElementById("sumupemmaos")
   sumup = Number(sumup.value)
-
+  //recebendo as informações do input
   let pagseguro = document.getElementById("pagseguroemmaos")
   pagseguro = Number(pagseguro.value)
-
+  //recebendo as informações do input
   let ninenine = document.getElementById("nineemmaos")
   ninenine = Number(nineemmaos.value)
 
-  let soma = dinheiro + ifood + pix + sumup + pagseguro + ninenine 
-
-
-
+  //Variável que soma VALORES CHECADOS
+  let soma = dinheiro + ifood + pix + sumup + pagseguro + ninenine
+  //console.log de teste de input
   console.log(dinheiro)
   console.log(ifood)
   console.log(pix)
