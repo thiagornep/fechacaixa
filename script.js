4705c8e276b52f98c34b08d90138c91350fb0a32
Thiago Rodrigues Sales <43177777+thiagornep@users.noreply.github.com>
2023-04-11 21:40:11 -0400

    Correção FINAL VALORES CHECADOS = SOMA 😎

--- script.js
+++ script.js
@@ -1,25 +1,38 @@
 function verificar() {
   window.alert("Testando a conexão....")
-  var dinheiropraformatar = document.getElementById("dinheiroemmaos")
-  var dinheiro = parseFloat(dinheiropraformatar.value)
-  var ifoodformatar = document.getElementById("ifoodemmaos")
-  var ifood = parseFloat(ifoodformatar.value)
-  var pixformatar = document.getElementById("pixemmaos")
-  var pix = parseFloat(pixformatar.value)
-  var sumupformatar = document.getElementById("dinheiroemmaos")
-  var sumup = parseFloat(sumupformatar.value)
-  var pagseguroformatar = document.getElementById("dinheiroemmaos")
-  var pagseguro = parseFloat(pagseguroformatar.value)
-  var ninefoodformatar = document.getElementById("dinheiroemmaos")
-  var ninefood = parseFloat(ninefoodformatar.value)
 
-  var soma =
-    parseFloat(dinheiropraformatar.value) +
-    parseFloat(ifoodformatar.value) +
-    parseFloat(pixformatar.value) +
-    parseFloat(sumupformatar.value) +
-    parseFloat(pagseguroformatar.value) +
-    parseFloat(ninefoodformatar.value)
+  let dinheiro = document.getElementById("dinheiroemmaos")
+  dinheiro = Number(dinheiro.value)
+
+  let ifood = document.getElementById("ifoodemmaos")
+  ifood = Number(ifood.value)
+
+  let pix = document.getElementById("pixemmaos")
+  pix = Number(pix.value)
+
+  let sumup = document.getElementById("sumupemmaos")
+  sumup = Number(sumup.value)
+
+  let pagseguro = document.getElementById("pagseguroemmaos")
+  pagseguro = Number(pagseguro.value)
+
+  let ninenine = document.getElementById("nineemmaos")
+  ninenine = Number(nineemmaos.value)
+
+  let soma = dinheiro + ifood + pix + sumup + pagseguro + ninenine 
+
+
+
+  console.log(dinheiro)
+  console.log(ifood)
+  console.log(pix)
+  console.log(sumup)
+  console.log(pagseguro)
+  console.log(ninenine)
+  console.log(soma)
+
+  // var soma = parseFloat(dinheiro) + parseFloat(ifood)
+  //soma = soma.toFixed(2)
+
   res.innerHTML = `Detectamos essa quantia em Dinheiro: R$  ${soma} .`
-  
 }
